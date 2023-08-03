0088d67897e486cb240bbbd54ef406497f51226a
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-03 19:56:02 +0300

    Basic annotations - floor

---
+++ 2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Floor a number"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+        This function implements the floor operation
+        and returns an int
+
+        @Args:
+            n: float
+    """
+    return math.floor(n)
