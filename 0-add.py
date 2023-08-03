f7ef4e664999cbbf933af6e324dc85955b1604da
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-03 19:17:24 +0300

    Basic annotations-add

---
+++ 0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    This module implements an add function that adds two numbers and returns
+    their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        This function adds two numbers together and returns their sum
+        @Args:
+            a: Argument one which is a float
+            b: Argument two which is a float
+    """
+    return a + b
