8309097fb8361c8e97b5e8b909e912d71d93664f
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-03 20:23:28 +0300

    Added task 0 - task 4 files to appropriate dir

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    This module contains a functional implementation of the
+    concatenation of two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        This function implements the functionality of
+        concatenating two strings together
+        @Args:
+            str1: A string
+            str2: A string
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Convert a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """
+        @Args:
+            n: float
+
+        @return_type:
+            n: str
+    """
+    return str(n)
