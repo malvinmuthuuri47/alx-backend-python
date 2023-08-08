04b2350b54fb39ff13a02050e9a02eb57023adcb
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-08 15:06:20 +0300

    Added generator return type for the async_generator()

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Async Generator"""
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An async func that takes no arguments, executes a loop 10 times, each
     time asynchronously waits 1 second, then yields a random number between
