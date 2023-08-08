09a58354f8cc76f4294e5a66217e64c383fc1c1b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-08 11:31:02 +0300

    Fixed return type hint error for task_wait_random()

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Tasks"""
 import asyncio
-from typing import Task
+from typing import Coroutine, Any, Type
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
+def task_wait_random(max_delay: int) -> Coroutine[Any, Any, Type[asyncio.Task]]:
     """
     This function takes an integer and returns a asyncio.Task
     """
