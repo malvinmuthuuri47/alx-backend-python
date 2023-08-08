2dc30249708f7a1105b49b897befe140d9075435
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-08 11:27:01 +0300

    Fixed wrong type hint for function tasks()

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Tasks"""
 import asyncio
+from typing import Task
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> any:
+def task_wait_random(max_delay: int) -> Task:
     """
     This function takes an integer and returns a asyncio.Task
     """
