b9f02f417eb9d17f96bf860e3aa0d599d3aadec9
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-08-08 11:32:16 +0300

    Corrected wrong type hint fot 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Executing multiple coroutines at the same time with async"""
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
         A function that takes two params, n and max_delay and spawns
         wait_random n times with the specified max_delay by passing
